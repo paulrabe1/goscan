93a82449728190bbf7d96aa2306e97c361a26807
paulrabe1 <voronovpavel2307@yandex.ru>
2019-08-11 16:06:14 +0200

    Rework using external lib

--- goscan.go
+++ goscan.go
@@ -8,12 +8,13 @@ import (
     "strconv"
     "time"
     "strings"
+    "github.com/korovkin/limiter"
 )
 
 var errors int = 0
 const numberOfPorts int = 65535
 
-func scanport(ip string, port string, timeout time.Duration) {
+func ScanPort(ip string, port string, timeout time.Duration) {
     target := fmt.Sprintf("%s:%s", ip, port)
     conn, err := net.DialTimeout("tcp", target, timeout)
     if err != nil {
@@ -23,7 +24,7 @@ func scanport(ip string, port string, timeout time.Duration) {
                 fmt.Println("Too many errors!!! Consider decreasing number of threads!")
                 os.Exit(1)
             }
-            scanport (ip, port, timeout)
+            ScanPort (ip, port, timeout)
         }
         return
     }
@@ -31,37 +32,25 @@ func scanport(ip string, port string, timeout time.Duration) {
     fmt.Println(port, "open")
 }
 
-func worker(id int, jobs <-chan int, results chan<- int, ip string, timeout time.Duration) {
-    for j := range jobs {
-        port := strconv.Itoa(j)
-        scanport(ip, port, timeout)
-        results <- j * 2
-    }
-}
-
 func main() {
     if len(os.Args) == 4 {
         ip := os.Args[1]
         threads, _ := strconv.Atoi(os.Args[2])
-        numOfMilliseconds, _ := strconv.Atoi(os.Args[3])
+        numOfMilliseconds, _ := strconv.Atoi(os.Args[3])   
         timeout := time.Duration(numOfMilliseconds)*time.Millisecond
 
-        jobs := make(chan int, numberOfPorts)
-        results := make(chan int, numberOfPorts)
+		limit := limiter.NewConcurrencyLimiter(threads)
 
-        for w := 1; w <= threads; w++ {
-            go worker(w, jobs, results, ip, timeout)
-        }
+		for i := 1; i < 65535; i++ {
+			port := strconv.Itoa(i)
+			limit.Execute(func() {
+					ScanPort(ip, port, timeout)
+  				})
+  		}
+  		limit.Wait()
 
-        for j := 1; j <= numberOfPorts; j++ {
-            jobs <- j
-        }
-        close(jobs)
-
-        for a := 1; a <= numberOfPorts; a++ {
-            <-results
-        }   
     } else {
         fmt.Println("Usage: ip threads timeout(milliseconds)")
     }
 }
+
